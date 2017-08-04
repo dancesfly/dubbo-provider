c5cc79eae231b14f219ea1cd552a766b387bde30
dancesfly <dancesfly@126.com>
2017-08-04 19:51:47 +0800

    commit

---
+++ src/main/java/com/test/provider/DubboProductorTest.java
@@ -0,0 +1,5 @@
+package com.test.provider;
+
+public interface DubboProductorTest {  
+    public void sayHello();  
+}  
---
+++ src/main/java/com/test/provider/DubboProductorTestImpl.java
@@ -0,0 +1,7 @@
+package com.test.provider;
+
+public class DubboProductorTestImpl implements DubboProductorTest{  
+    public void sayHello(){  
+        System.out.println("这是我的第一个dubbo测试");  
+    }  
+}  
